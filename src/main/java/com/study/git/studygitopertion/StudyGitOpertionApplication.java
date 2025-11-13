1a18265fab1607a4dff0bbf51935366f603f47aa
functionhan <1057574553@qq.com>
2025-11-13 22:10:27 +0800

    feat: init project base files

---
+++ src/main/java/com/study/git/studygitopertion/StudyGitOpertionApplication.java
@@ -0,0 +1,13 @@
+package com.study.git.studygitopertion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyGitOpertionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyGitOpertionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/git/studygitopertion/StudyGitOpertionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.git.studygitopertion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyGitOpertionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
